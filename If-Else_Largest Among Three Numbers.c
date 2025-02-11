bf46751556b082437046b8eaae10fcca0ded3fe1
Ambika0112 <ambikamalgaya2@gmail.com>
2025-02-11 09:21:19 +0530

    second question

---
+++ If-Else_Largest Among Three Numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+   int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    if(a>b && a>c){
+        printf("The largest number is : %d",a);
+    }
+    else if(b>c && b>a){
+        printf("The largest number is : %d",b);
+    }
+    else if(c>b && c>a){
+        printf("The largest number is : %d",c);
+    }
+  else{
+       printf("The largest number is : %d",c);
+  }
+
+    
+    return 0;
+}
